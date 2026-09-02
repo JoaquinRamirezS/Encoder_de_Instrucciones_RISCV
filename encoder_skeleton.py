0738dccb67d963d123ef6580ef928a2079f47a43
JoaquinRamirezS <jramirezsequeira@gmail.com>
2026-09-01 18:16:23 -0600

    [FEAT]: Definiciónn de instruction code y codificación de formato R

--- encoder_skeleton.py
+++ encoder_skeleton.py
@@ -31,6 +31,97 @@ def encode_instruction(instruction: str) -> int:
     # TODO: implementar. Sugerencia: parsear el mnemónico y los operandos,
     # despachar según el formato (R/I/S/B), y ensamblar los campos con
     # operaciones de bits.
+
+    # Eliminar espacios al principio y final de la instruccion para evitar errores
+    instruction_name, operands = instruction.strip().split(maxsplit=1)
+    # Eliminar espaciones entre operandos 
+    operands = operands.replace(" ", "")
+
+    # Valores que establece RISC-V   para cada instrucción.
+    instruction_codes = {
+
+     # Instrucciones formato R     
+     "add":{"opcode": 0b0110011, "funct3": 0b000, "funct7": 0b0000000},
+     "sub":{"opcode": 0b0110011, "funct3": 0b000, "funct7": 0b0100000},
+     "and":{"opcode": 0b0110011, "funct3": 0b111, "funct7": 0b0000000},
+     "or":{"opcode": 0b0110011, "funct3": 0b110, "funct7": 0b0000000},
+
+    # Istrucciones formato I(aritmetica)
+    "addi":{"opcode": 0b0010011, "funct3": 0b000, "funct7": None},
+    "andi":{"opcode": 0b0010011, "funct3": 0b111, "funct7": None},
+
+    # Instrucciones formato I(carga)
+    "lw":{"opcode": 0b0000011, "funct3": 0b010, "funct7": None},
+    "lb":{"opcode": 0b0000011, "funct3": 0b000, "funct7": None},
+
+    # Instrucciones formato S
+    "sw":{"opcode": 0b0100011, "funct3": 0b010, "funct7": None},
+    "sb":{"opcode": 0b0100011, "funct3": 0b000, "funct7": None},
+
+    # Instrucciones formato B
+    "beq":{"opcode": 0b1100011, "funct3": 0b000, "funct7": None},
+    "bne":{"opcode": 0b1100011, "funct3": 0b001, "funct7": None}
+    }
+
+    # Verificar que se encuentre la instruccion en instructions code
+    if instruction_name not in instruction_codes:
+        raise ValueError("La instrucción no es soportada.Revisar lista de soportadas")
+
+    # Obtener códigos de instrucción
+    opcode = instruction_codes[instruction_name]["opcode"]
+    funct3 = instruction_codes[instruction_name]["funct3"]
+    funct7 = instruction_codes[instruction_name]["funct7"]
+
+    # Parsear operandos
+    operands_separate = operands.split(',') # Se separan por comas
+
+    # Convertir los registros de texto a numero entero
+    # Como x5 a solo 5
+    operands_numbers=[] # Lista para guardar el numero del operando (x5 se guarda solo el 5)
+
+    for operand in operands_separate:
+        if operand.startswith("x"):
+            operands_numbers.append(int(operand[1:]))
+        else:
+            operands_numbers.append(int(operand))
+
+    # Formato R
+    if instruction_name in ['add','sub','and','or']:
+
+        #Obtener los registros
+        rd = operands_numbers[0]
+        rs1 = operands_numbers[1]
+        rs2 = operands_numbers[2]
+
+        # Colocar cada campo según el formato de la instrucción
+        encoded = (
+            (funct7 << 25) | #Bits del 31 al 25
+            (rs2 << 20) |    #Bits del 24 al 20
+            (rs1 << 15) |    #Bits del 19 al 15
+            (funct3 << 12) | #Bits del 14 al 12
+            (rd << 7) |      #Bits del 11 al 7
+            (opcode)         #Bits del 6 al 0
+        )
+
+        return encoded
+
+    # Formato I(aritmetica)
+    if instruction_name in ['addi','andi']:
+        return 
+    
+    # Formato I(carga)
+    if instruction_name in ['lw','lb']:
+        return 
+    
+    # Formato S
+    if instruction_name in ['sw','sb']:
+        return 
+    
+    # Formato B
+    if instruction_name in ['bne','beq']:
+        return 
+    
+
     raise NotImplementedError("encode_instruction: pendiente de implementar")
 
 
